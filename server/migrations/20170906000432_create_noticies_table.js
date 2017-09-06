69ec324c50ff090699827e55b9f0e47ef2285371
João Julio <joaopjt@gmail.com>
2017-09-06 00:09:35 -0300

    Added migration to create notiices table for project

---
+++ server/migrations/20170906000432_create_noticies_table.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  knex.createIfNotExists('noticies', function(t) {
+    t.increments();
+    t.string('title');
+    t.string('subtitle');
+    t.string('content');
+    t.string('image');
+    t.timestamps(true, true);
+  })
+    .then(function() {
+      console.log("CREATED 'noticies' table with success!");
+    })
+    .catch(function(err) {
+      console.log("ERROR at try to create 'noticies' table! ", err);
+    });
+};
+
+exports.down = function(knex, Promise) {
+  knex.dropTableIfExists('noticies')
+    .then(function() {
+      console.log("DROPPED 'noticies' table with success!");
+    })
+    .catch(function(err) {
+      console.log("ERROR at try to drop 'noticies' table! ", err);
+    });
+};
